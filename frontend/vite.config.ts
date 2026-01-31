192edbe6b028a5de3bff7a00368b6ed4ebb0bc62
Gustavo Detomi <gustavodetomi@gmail.com>
2026-01-31 14:50:46 -0300

    Ajustes no init do projeto frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3333',  // Backend está aqui
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
